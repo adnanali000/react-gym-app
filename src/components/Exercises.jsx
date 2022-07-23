11bc81c081fee49125d7cf73db449e32e1b53b95
adnanali000 <adnan.museel123@gmail.com>
2022-07-23 09:47:18 +0500

    exercises search work done

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React,{ useState, useEffect} from 'react'
+import Pagination from '@mui/material/Pagination'
+import {Box,Typography,Stack} from '@mui/material'
+import ExerciseCard from '../components/ExerciseCard'
+import {ArrowUpward} from '@mui/icons-material'
 
-const Exercises = () => {
+const Exercises = ({exercises,setExercises,bodyPart}) => {
+  const [currentPage,setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise,indexOfLastExercise)
+
+  const paginate = (e,value) => {
+    setCurrentPage(value)
+
+    window.scrollTo({ top:1800, behaviour:'smooth'})
+  }
+
   return (
-    <div>Exercises</div>
+    <Box id="exercises"
+      sx={{ mt: {lg: '110px'}}}
+      mt="50px"
+      p="20px"  
+    >
+      {exercises.length >= 1 ? (
+        <Typography variant="h4" mb="45px">
+          Showing Results
+        </Typography>
+        ): (
+          <Typography variant="h4" mb="5px">
+          Search Exercises <ArrowUpward />
+        </Typography>
+        )}
+
+      <Stack direction="row" sx={{gap: {lg: '110px', xs:'50px'}}} flexWrap="wrap" justifyContent="center">
+        {currentExercises.map((exercise,index)=>(
+          <ExerciseCard exercise={exercise} key={index} />
+        ))}
+      </Stack>
+      
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination 
+          shape="rounded"
+          color="standard"
+          defaultPage={1}
+          count={Math.ceil(exercises.length / exercisesPerPage)}
+          page={currentPage}
+          onChange={paginate}
+          size="large"
+          />
+          )}
+      </Stack>
+
+    </Box>
   )
 }
 
