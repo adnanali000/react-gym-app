af55bedd7e3ef69f992bb602b001909f273226e3
adnanali000 <adnan.museel123@gmail.com>
2022-07-23 20:34:31 +0500

    AA fitness work done

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ const App = () => {
   return (
     <Box width="400px" sx={{ width: { xl: '1488px'}}} m="auto">
         <Navbar />
+        {/* routes */}
         <Routes>
             <Route path = "/" element={<Home />} />
             <Route path = "/exercise/:id" element={<ExerciseDetail />} />
         </Routes>
+        
         <Footer />
     </Box>
   )
