7ed88fd318a0b352f7072b800c68fff1dc3f79af
adnanali000 <adnan.museel123@gmail.com>
2022-07-23 12:32:33 +0500

    exercise video component work done

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import {Box,Typography,Stack} from '@mui/material'
 
-const ExerciseVideos = () => {
+const ExerciseVideos = ({exerciseVideos,name}) => {
   return (
-    <div>ExerciseVideos</div>
+    <Box sx={{ marginTop: {lg: '130px',xs:'20px'}}} p="20px">
+      <Typography variant="h3" mb="10px">
+        Watch <span style={{color:'#ff2625',textTransform:'capitalize'}}>{name}</span> exercise videos
+      </Typography>
+      <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center" 
+        sx={{
+          flexDirection: {lg: 'row'},
+          gap: { lg: '80px', xs: '0'}
+        }}
+      >
+        {exerciseVideos?.slice(0,6).map((item,index)=>(
+          <a
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+            target="_blank"
+            rel="noreffer"
+          >
+            <img src={item.video.thumbnails[0].url} alt={item.video.title} style={{width:'100%',height:'100%',objectFit:'contain'}} />
+            
+            <Box>
+              <Typography sx={{fontSize: {lg: '20px',xs:'18px'}}} color="#000">
+                {item.video.title}
+              </Typography>
+              <Typography sx={{fontSize: {lg: '18px',xs:'16px'},color:'gray'}}>
+                {item.video.channelName}
+              </Typography>
+
+            </Box>
+          </a>
+        ))}
+
+      </Stack>
+    </Box>
   )
 }
 
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,13 +1,14 @@
 import React,{useState,useEffect} from 'react'
 import {Box} from '@mui/material'
 import {useParams} from 'react-router-dom'
-import {fetchData,exerciseOptions} from '../utils/fetchData'
+import {fetchData,exerciseOptions,youtubeOptions} from '../utils/fetchData'
 import Detail from '../components/Detail'
 import ExerciseVideos from '../components/ExerciseVideos'
 import SimilarExercises from '../components/SimilarExercises'
 
 const ExerciseDetail = () => {
   const [exerciseDetail,setExerciseDetail] = useState({});
+  const [exerciseVideos,setExerciseVideos] = useState([]);
   const {id} = useParams();
 
   useEffect(()=>{
@@ -17,6 +18,10 @@ const ExerciseDetail = () => {
 
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,exerciseOptions)
       setExerciseDetail(exerciseDetailData)
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,youtubeOptions)
+      setExerciseVideos(exerciseVideosData.contents)
+
     }
 
     fetchExercisesData()
@@ -25,7 +30,7 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail = {exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos exerciseVideos = {exerciseVideos} name={exerciseDetail.name} />
       <SimilarExercises />
     </Box>
   )
